0c3bcfaac4776ee73987576c9cc84f3452c355c0
ashok parihar <aparihar@email.arizona.edu>
2024-03-24 22:09:34 -0600

    Adding the prompt asking the user what to do.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
 
-// Connecting to database
+// Connecting to the database
 const db = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -18,3 +18,51 @@ db.connect((err) => {
     // Starting the application
     start();
 });
+
+// Function to Start SQL Employee Tracker Application
+function start() {
+    inquirer.prompt({
+            type: "list",
+            name: "action",
+            message: "What would you like to do?",
+            choices: [
+                "View all departments",
+                "View all roles",
+                "View all employees",
+                "Add a department",
+                "Add a role",
+                "Add an employee",
+                "Update an employee role",
+                "Exit",
+            ],
+        })
+        .then((answer) => {
+            switch (answer.action) {
+                case "View all departments":
+                    viewAllDepartments();
+                    break;
+                case "View all roles":
+                    viewAllRoles();
+                    break;
+                case "View all employees":
+                    viewAllEmployees();
+                    break;
+                case "Add a department":
+                    addDepartment();
+                    break;
+                case "Add a role":
+                    addRole();
+                    break;
+                case "Add an employee":
+                    addEmployee();
+                    break;
+                case "Update an employee role":
+                    updateEmployeeRole();
+                    break;
+                case "Exit":
+                    console.log("Goodbye!");
+                    db.end();
+                    break;
+            }
+        });
+}
